87c653fce7ca3e9590a22ee0bd4db75b7de128e2
Leandro Gomes <107895852+leandrog20@users.noreply.github.com>
2025-07-13 21:47:21 -0300

    Aula sobre como definir o model de categorias

---
+++ 24. Definindo o model de categorias/models/Categoria.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const categoria = new Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+
+    slug: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
