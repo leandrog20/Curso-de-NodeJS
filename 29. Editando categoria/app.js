cd3ea9e35d5fbee4959273b58ceeac3695411590
Leandro Gomes <107895852+leandrog20@users.noreply.github.com>
2025-07-16 20:17:56 -0300

    Aula aprendendo a como editar as nossas categorias

---
+++ 29. Editando categoria/app.js
@@ -0,0 +1,67 @@
+// Carregando módulos necessários
+    import express from 'express';
+    import { engine } from 'express-handlebars';
+    import bodyParser from 'body-parser';
+    import mongoose from 'mongoose';
+    import admin from './routes/admin.js';
+    import path from 'path';
+    import { fileURLToPath } from 'url';
+    import session from 'express-session';
+    import flash from 'connect-flash';
+import { nextTick } from 'process';
+
+    const app = express();
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+// Configurações
+    // Sessão
+    app.use(session({
+        secret: 'cursodenode',
+        resave: true,
+        saveUninitialized: true
+    }));
+
+    app.use(flash());
+
+    // Middleware
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash('success_msg')
+        res.locals.error_msg = req.flash('error_msg')
+        next();
+    })
+
+    // Body Parser
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    // Handlebars
+    app.engine('handlebars', engine({ defaultLayout: 'main', runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true
+    }}));
+    app.set('view engine', 'handlebars');
+
+    // Mongoose
+    mongoose.connect('mongodb://localhost/blogapp').then(() => {
+        console.log('Conectado ao MongoDB');
+    
+       }).catch((err) => {
+        console.log('Erro ao conectar ao MongoDB: ' + err);
+       })
+
+
+    // Public
+    app.use(express.static(path.join(__dirname, 'public')));
+// Rotas
+app.get('/', (req, res) => {
+    res.send('Página Inicial');
+});
+app.use('/admin', admin);
+
+// Outros
+
+const PORT = 6500;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+})
