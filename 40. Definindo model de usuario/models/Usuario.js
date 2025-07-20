585b12a066417d160118748b7c3ab77e29189360
Leandro Gomes <107895852+leandrog20@users.noreply.github.com>
2025-07-20 20:50:31 -0300

    Iniciando o módulo de autenticação de usuário

---
+++ 40. Definindo model de usuario/models/Usuario.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const usuarioSchema = new Schema({
+    nome: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    senha: {
+        type: String,
+        required: true,
+    },
+})
+
+export default mongoose.model("Usuario", usuarioSchema);
